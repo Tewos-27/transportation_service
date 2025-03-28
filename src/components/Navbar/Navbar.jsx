c6545eb655e4f9ab173d322f18caf160e0e084d8
Tewos-21 <tewodrosshimels54@gmail.com>
2025-03-28 00:57:53 -0700

    Add a navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react'
+import Link from 'react-router-dom'
+const Navbar = () => {
+  return (
+    <div>
+        <ul className='navbar'>
+            <Link to=''><li></li></Link>
+            <Link to=''><li></li></Link>
+            <Link to=''><li></li></Link>
+            <Link to=''><li></li></Link>
+        </ul>
+    </div>
+  )
+}
+
+export default Navbar
